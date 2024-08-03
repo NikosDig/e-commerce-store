d85fdb204fee85c0f348522aee4f4e2f64e36d50
NikosDigalakis <nikos_23_chania@hotmail.com>
2024-08-03 17:28:24 +0200

    created the rest of the pages needed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { Routes,Route,Link } from "react-router-dom";
+import Layout from "./components/Router/Layout";
+import Home from "./components/HomePage";
+import Products from "./components/ProductsPage";
+import RouteNotFound from "./components/Router/RouteNotFound";
 
 function App() {
-  return 
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Route>
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
